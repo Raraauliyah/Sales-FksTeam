d12c3b7e51a954aa7951fabfa6be6e3067a12a6d
Krushndevsinh Jadeja <krushndevsinhjadeja@gmail.com>
2019-08-16 08:51:17 +0530

    [FIX] singleton error fixed

--- ping_modifier_sales/models/res_users.py
+++ ping_modifier_sales/models/res_users.py
@@ -14,13 +14,14 @@ class ResUsers(models.Model):
     @api.multi
     def compute_count_confirmed_orders_today(self):
         sobj = self.env['sale.order']
-        self.count_confirmed_orders_today = 0
-        orders = sobj.search([('operator_id', '=', self.id), ('state', '=', 'sale'), ('state_ws', '!=', 'completed')])
-        for order in orders:
-            confirmation_date = order.confirmation_date.split(' ')[0]
-            today = datetime.today().strftime('%Y-%m-%d')
-            if today == confirmation_date:
-                self.count_confirmed_orders_today += 1
+        for rec in self:
+            rec.count_confirmed_orders_today = 0
+            orders = sobj.search([('operator_id', '=', rec.id), ('state', '=', 'sale'), ('state_ws', '!=', 'completed')])
+            for order in orders:
+                confirmation_date = order.confirmation_date.split(' ')[0]
+                today = datetime.today().strftime('%Y-%m-%d')
+                if today == confirmation_date:
+                    rec.count_confirmed_orders_today += 1
 
 
 class WorkingScheduleLine(models.Model):
